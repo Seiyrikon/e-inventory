2d8f53326140ba8570d36e2ff893cd740698cec4
Seiyrikon <rickygalpo7777@gmail.com>
2026-02-15 05:36:42 +0800

    Initial codes

---
+++ Codebase/e-inventory_backend/e-inventory/src/test/java/seiyrikon/com/e_inventory/EInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package seiyrikon.com.e_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
